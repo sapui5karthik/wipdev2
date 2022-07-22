d91bcf119ad9eeb8ceea1de50d778c85cb7e30c5
sapui5karthik <sapui5training99@gmail.com>
2022-07-22 15:33:57 +0000

    screen2 table

--- wipproject2/webapp/controller/Object.controller.js
+++ wipproject2/webapp/controller/Object.controller.js
@@ -2,8 +2,13 @@ sap.ui.define([
     "./BaseController",
     "sap/ui/model/json/JSONModel",
     "sap/ui/core/routing/History",
-    "../model/formatter"
-], function (BaseController, JSONModel, History, formatter) {
+    "../model/formatter",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+    "sap/m/MessageToast",
+    "sap/m/MessageBox"
+
+], function (BaseController, JSONModel, History, formatter,Filter,FilterOperator,MessageToast,MessageBox) {
     "use strict";
 
     return BaseController.extend("com.chappota.wippoc2.wipproject2.controller.Object", {
@@ -13,12 +18,34 @@ sap.ui.define([
         /* =========================================================== */
         /* lifecycle methods                                           */
         /* =========================================================== */
+        onInit : function(){
+            this.oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+            this.oRouter.getRoute("object").attachPatternMatched(this._getwipprojectdata,this);
+        },
+        _getwipprojectdata : function(oevent){
+            var pid = oevent.getParameter("arguments").pid;
+            
+            var jrnlentrymdl = this.getOwnerComponent().getModel("jrnlentryMDL");
+            var projectfilter = new Filter("Project",FilterOperator.EQ,pid);
+            jrnlentrymdl.read("/YY1_JournalEntryItem",{
+                filters : [projectfilter],
+                success : function(odata){
+                    var jsonmodel = new JSONModel();
+                    jsonmodel.setData(odata.results);
+                    debugger;
+                    this.getView().byId("wiptable").setModel(jsonmodel,"wipentry");
+                }.bind(this),
+                error : function(msg){
+
+                }.bind(this)
+            });
+        },
 
         /**
          * Called when the worklist controller is instantiated.
          * @public
          */
-        onInit : function () {
+        _onInit : function () {
             // Model used to manipulate control states. The chosen values make sure,
             // detail page shows busy indication immediately so there is no break in
             // between the busy indication for loading the view's meta data
